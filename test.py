34c01243773c8c836528f8ffc1b45d5615b58ac7
Akuqt <bestor911@gmail.com>
2024-12-14 15:47:11 -0500

    chore: update testing file

--- test.py
+++ test.py
@@ -1,15 +1,32 @@
-from src import Express, Response, Request
+from src import Express, Response, Request, Router
 
 
 def get(req: Request, res: Response):
-    # print(req.headers)
-    res.json({
-        "Hello": "World!"
-    })
+    res.json({"Hello": "World!"})
 
+
+def get_test(req: Request, res: Response):
+    res.json({"test": "Hello World!"})
+
+
+def middleware1(req: Request, res: Response, next):
+    res.add_header("X-Test-1", "Middleware")
+    next()
+
+
+def middleware2(req: Request, res: Response, next):
+    res.add_header("X-Test-2", "Middleware")
+    next()
+
+
+router = Router()
+
+router.get("/test/a", middleware1, middleware2, get_test)
 
 app = Express()
 
-app.get("/", get)
+app.use(router)
+
+app.get("/", None, get)
 
 app.listen(cb=lambda h, p: print(f"http://{h}:{p}"))
